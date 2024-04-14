d30e11d73e80a11267960a3afd29a824f7d5ffb2
Kaushik Sharma <kaushik445566@gmail.com>
2024-04-15 01:42:54 +0530

    fix: minor bug fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,22 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
+import { Suspense } from "react";
 
+import { Loading } from "@/components/auth/loading";
 import ConvexClientProvider from "@/providers/convex-client-provider";
 
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
 import { ModalProvider } from "@/providers/modal-provider";
+import { siteConfig } from "@/config";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "CoVisual",
-  description: "Real-Time Collaborative Whiteboard",
-};
+export const metadata: Metadata = siteConfig;
 
+export const viewport: Viewport = {
+  themeColor: "#fff",
+};
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -22,12 +25,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ConvexClientProvider>
-          <Toaster theme="light" closeButton richColors />
-          <ModalProvider />
-
-          {children}
-        </ConvexClientProvider>
+        <Suspense fallback={<Loading />}>
+          <ConvexClientProvider>
+            <Toaster theme="light" closeButton richColors />
+            <ModalProvider />
+            {children}
+          </ConvexClientProvider>
+        </Suspense>
       </body>
     </html>
   );
---
+++ config/index.ts
@@ -0,0 +1,49 @@
+import type { Metadata } from "next";
+
+export const siteConfig: Metadata = {
+  title: "CoVisual",
+  description: "Real-time collaboration, versatile canvas, rich media, secure.",
+  keywords: [
+    "reactjs",
+    "nextjs",
+    "vercel",
+    "react",
+    "miro",
+    "board",
+    "shadcn",
+    "shadcn-ui",
+    "radix-ui",
+    "cn",
+    "clsx",
+    "draw",
+    "convex",
+    "sonner",
+    "liveblocks",
+    "zod",
+    "nosql",
+    "lucide-react",
+    "next-themes",
+    "clerk-themes",
+    "clerk",
+    "postcss",
+    "prettier",
+    "react-dom",
+    "tailwindcss",
+    "tailwindcss-animate",
+    "ui/ux",
+    "js",
+    "javascript",
+    "typescript",
+    "eslint",
+    "html",
+    "css",
+  ] as Array<string>,
+  authors: {
+    name: "Kaushik Sharma",
+    url: "https://github.com/ikaushiksharma",
+  },
+} as const;
+
+export const links = {
+  sourceCode: "https://github.com/ikaushiksharma/covisual",
+} as const;
