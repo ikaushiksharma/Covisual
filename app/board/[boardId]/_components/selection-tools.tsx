014fcbd7c820e509e7afc5d57eb21eed43381164
Kaushik Sharma <kaushik445566@gmail.com>
2024-04-14 19:36:15 +0530

    feat: selection layer functionality

--- app/board/[boardId]/_components/selection-tools.tsx
+++ app/board/[boardId]/_components/selection-tools.tsx
@@ -1,7 +1,10 @@
 "use client";
-
+import { BringToFront, SendToBack, Trash2 } from "lucide-react";
 import { memo } from "react";
 
+import { Button } from "@/components/ui/button";
+import { Hint } from "@/components/hint";
+import { useDeleteLayers } from "@/hooks/use-delete-layers";
 import { useSelectionBounds } from "@/hooks/use-selection-bounds";
 import { useMutation, useSelf } from "@/liveblocks.config";
 import type { Camera, Color } from "@/types/canvas";
@@ -15,6 +18,42 @@ type SelectionToolsProps = {
 
 export const SelectionTools = memo(({ camera, setLastUsedColor }: SelectionToolsProps) => {
   const selection = useSelf((me) => me.presence.selection);
+  const moveToFront = useMutation(
+    ({ storage }) => {
+      const liveLayerIds = storage.get("layerIds");
+      const indices: number[] = [];
+
+      const arr = liveLayerIds.toArray();
+
+      for (let i = 0; i < arr.length; i++) {
+        if (selection.includes(arr[i])) indices.push(i);
+      }
+
+      for (let i = indices.length - 1; i >= 0; i--) {
+        liveLayerIds.move(indices[i], arr.length - 1 - (indices.length - 1 - i));
+      }
+    },
+    [selection],
+  );
+
+  const moveToBack = useMutation(
+    ({ storage }) => {
+      const liveLayerIds = storage.get("layerIds");
+      const indices: number[] = [];
+
+      const arr = liveLayerIds.toArray();
+
+      for (let i = 0; i < arr.length; i++) {
+        if (selection.includes(arr[i])) indices.push(i);
+      }
+
+      for (let i = 0; i < indices.length; i++) {
+        liveLayerIds.move(indices[i], i);
+      }
+    },
+    [selection],
+  );
+
   const setFill = useMutation(
     ({ storage }, fill: Color) => {
       const liveLayers = storage.get("layers");
@@ -26,6 +65,8 @@ export const SelectionTools = memo(({ camera, setLastUsedColor }: SelectionTools
     },
     [selection, setLastUsedColor],
   );
+
+  const deleteLayers = useDeleteLayers();
 
   const selectionBounds = useSelectionBounds();
 
@@ -45,6 +86,25 @@ export const SelectionTools = memo(({ camera, setLastUsedColor }: SelectionTools
       }}
     >
       <ColorPicker onChange={setFill} />
+      <div className="flex flex-col gap-y-0.5">
+        <Hint label="Bring to front">
+          <Button onClick={moveToFront} variant="board" size="icon">
+            <BringToFront />
+          </Button>
+        </Hint>
+        <Hint label="Bring to back" side="bottom">
+          <Button onClick={moveToBack} variant="board" size="icon">
+            <SendToBack />
+          </Button>
+        </Hint>
+      </div>
+      <div className="flex items-center pl-2 ml-2 border-l border-t-neutral-200">
+        <Hint label="Delete">
+          <Button variant="board" size="icon" onClick={deleteLayers}>
+            <Trash2 />
+          </Button>
+        </Hint>
+      </div>
     </div>
   );
 });
