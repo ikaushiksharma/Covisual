7b1758779cc71d4c8058667055449cf9587d9197
Kaushik Sharma <kaushik445566@gmail.com>
2024-04-14 19:09:19 +0530

    feat: cursor placement functionality

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -1,9 +1,10 @@
 "use client";
-import { useState } from "react";
+import React, { useCallback, useState } from "react";
 
-import { CanvasMode, type CanvasState } from "@/types/canvas";
-
-import { useCanRedo, useCanUndo, useHistory } from "@/liveblocks.config";
+import { type Camera, CanvasMode, type CanvasState } from "@/types/canvas";
+import { pointerEventToCanvasPoint } from "@/lib/utils";
+import { useCanRedo, useCanUndo, useHistory, useMutation } from "@/liveblocks.config";
+import { CursorsPresence } from "./cursors-presence";
 import { Info } from "./info";
 import { Participants } from "./participants";
 import { Toolbar } from "./toolbar";
@@ -16,10 +17,30 @@ export const Canvas = ({ boardId }: CanvasProps) => {
   const [canvasState, setCanvasState] = useState<CanvasState>({
     mode: CanvasMode.None,
   });
-
+  const [camera, setCamera] = useState<Camera>({ x: 0, y: 0 });
   const history = useHistory();
   const canUndo = useCanUndo();
   const canRedo = useCanRedo();
+  const onWheel = useCallback((e: React.WheelEvent) => {
+    setCamera((camera) => ({
+      x: camera.x - e.deltaX,
+      y: camera.y - e.deltaY,
+    }));
+  }, []);
+
+  const onPointerMove = useMutation(({ setMyPresence }, e: React.PointerEvent) => {
+    e.preventDefault();
+
+    const current = pointerEventToCanvasPoint(e, camera);
+
+    setMyPresence({ cursor: current });
+  }, []);
+
+  const onPointerLeave = useMutation(({ setMyPresence }) => {
+    setMyPresence({
+      cursor: null,
+    });
+  }, []);
   return (
     <main className="h-full w-full relative bg-neutral-100 touch-none">
       <Info boardId={boardId} />
@@ -32,6 +53,17 @@ export const Canvas = ({ boardId }: CanvasProps) => {
         undo={history.undo}
         redo={history.redo}
       />
+
+      <svg
+        className="h-[100vh] w-[100vw]"
+        onWheel={onWheel}
+        onPointerMove={onPointerMove}
+        onPointerLeave={onPointerLeave}
+      >
+        <g>
+          <CursorsPresence />
+        </g>
+      </svg>
     </main>
   );
 };
