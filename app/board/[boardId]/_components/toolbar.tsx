278237010e034dcff280611bbd7e95723a31e354
Kaushik Sharma <kaushik445566@gmail.com>
2024-04-14 19:00:24 +0530

    feat: canvas history

--- app/board/[boardId]/_components/toolbar.tsx
+++ app/board/[boardId]/_components/toolbar.tsx
@@ -8,10 +8,26 @@ import {
   Type,
   Undo2,
 } from "lucide-react";
-
+import type { CanvasState } from "@/types/canvas";
 import { ToolButton } from "./tool-button";
 
-export const Toolbar = () => {
+type ToolbarProps = {
+  canvasState: CanvasState;
+  setCanvasState: (newState: CanvasState) => void;
+  undo: () => void;
+  redo: () => void;
+  canUndo: boolean;
+  canRedo: boolean;
+};
+
+export const Toolbar = ({
+  canvasState,
+  setCanvasState,
+  undo,
+  redo,
+  canRedo,
+  canUndo,
+}: ToolbarProps) => {
   return (
     <div className="absolute top-[50%] -translate-y-[50%] left-2 flex flex-col gap-y-4">
       <div className="bg-white rounded-md p-1.5 flex gap-y-1 flex-col items-center shadow-md">
@@ -24,8 +40,8 @@ export const Toolbar = () => {
       </div>
 
       <div className="bg-white rounded-md p-1.5 flex flex-col items-center shadow-md">
-        <ToolButton label="Undo" icon={Undo2} onClick={() => {}} isDisabled={true} />
-        <ToolButton label="Redo" icon={Redo2} onClick={() => {}} isDisabled={true} />
+        <ToolButton label="Undo" icon={Undo2} onClick={undo} isDisabled={!canUndo} />
+        <ToolButton label="Redo" icon={Redo2} onClick={redo} isDisabled={!canRedo} />{" "}
       </div>
     </div>
   );
