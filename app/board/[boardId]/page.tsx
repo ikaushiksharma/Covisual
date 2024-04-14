7eeab925bc17af0521a54401e35656fa840921f3
Kaushik Sharma <kaushik445566@gmail.com>
2024-04-14 18:01:51 +0530

    feat: liveblocks setup done

--- app/board/[boardId]/page.tsx
+++ app/board/[boardId]/page.tsx
@@ -1,14 +1,18 @@
 import { Canvas } from "./_components/canvas";
+import { Room } from "@/components/room";
+import { Loading } from "./_components/loading";
 
 type BoardIdPageProps = {
-    params: {
-        boardId: string
-    }
-}
+  params: {
+    boardId: string;
+  };
+};
 
-const BoardIdPage = ({params}: BoardIdPageProps) => {
+const BoardIdPage = ({ params }: BoardIdPageProps) => {
   return (
+    <Room roomId={params.boardId} fallback={<Loading />}>
       <Canvas boardId={params.boardId} />
+    </Room>
   );
 };
 
