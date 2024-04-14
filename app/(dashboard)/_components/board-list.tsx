e87145bac4ab60b5d399c32aba52c83e62ab603f
Kaushik Sharma <kaushik445566@gmail.com>
2024-04-14 17:11:05 +0530

    feat: board card ui

--- app/(dashboard)/_components/board-list.tsx
+++ app/(dashboard)/_components/board-list.tsx
@@ -1,8 +1,13 @@
 "use client";
 
-import { EmptySearch } from "./empty-search";
-import { EmptyFavourites } from "./empty-favourites";
+import { useQuery } from "convex/react";
+
+import { api } from "@/convex/_generated/api";
+
+import { BoardCard } from "./board-card";
 import { EmptyBoards } from "./empty-boards";
+import { EmptyFavourites } from "./empty-favourites";
+import { EmptySearch } from "./empty-search";
 type BoardListProps = {
   orgId: string;
   query: {
@@ -12,7 +17,9 @@ type BoardListProps = {
 };
 
 export const BoardList = ({ orgId, query }: BoardListProps) => {
-  const data = []; // TODO: Change to API Call
+  const data = useQuery(api.boards.get, { orgId });
+
+  if (data === undefined) return <div>Loading...</div>;
 
   if (!data?.length && query.search) {
     return <EmptySearch />;
@@ -25,5 +32,25 @@ export const BoardList = ({ orgId, query }: BoardListProps) => {
   if (!data?.length) {
     return <EmptyBoards />;
   }
-  return <div className="">BoardList</div>;
+  return (
+    <div className="">
+      <h2 className="text-3xl">{query.favourites ? "Favourite boards" : "Team boards"}</h2>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
+        {data?.map((board) => (
+          <BoardCard
+            key={board._id}
+            id={board._id}
+            title={board.title}
+            imageUrl={board.imageUrl}
+            authorId={board.authorId}
+            authorName={board.authorName}
+            createdAt={board._creationTime}
+            orgId={board.orgId}
+            isFavourite={false}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
